7dfdfd44de380eb75d946c52cd965ac713ebdae9
Caio Coelho <caioz.co@gmail.com>
2022-03-01 19:47:30 -0500

    Remove duplicated comments

--- src/routes/recipient.route.ts
+++ src/routes/recipient.route.ts
@@ -12,74 +12,7 @@ class RecipientRoute implements Routes {
   }
 
   private initializeRoutes() {
-    /** GET data to populate a recipient's dashboard.
-     * @openapi
-     * /recipients/{id}:
-     *    get:
-     *      tags:
-     *      - Recipients
-     *      summary: Find User By Id
-     *      produces:
-     *      - "application/json"
-     *      parameters:
-     *      - name: id
-     *        in: path
-     *        description: Recipient Id
-     *        required: true
-     *        type: integer
-     *
-     *      responses:
-     *        200:
-     *          description: 'OK'
-     *          content:
-     *            application/json:
-     *              schema:
-     *                type: object
-     *                properties:
-     *                  recInfo:
-     *                    type: array
-     *                    items:
-     *                      type: object
-     *                      properties:
-     *                        id:
-     *                          type: integer
-     *                          example: 42
-     *                        name:
-     *                          type: string
-     *                          example: Donald trump
-     *                        ideology:
-     *                          type: float
-     *                          example: -0.15
-     *                        party:
-     *                          type: string
-     *                          example: "Republican Party"
-     *                  donationsByMonth:
-     *                    type: array
-     *                    items:
-     *                      type: object
-     *                      properties:
-     *                        month_start_date:
-     *                          type: string
-     *                          example: 2012-12-01T00:00:00.000Z
-     *                        amount_donated:
-     *                          type: float
-     *                          example: 12345.67
-     *                  topDonators:
-     *                    type: array
-     *                    items:
-     *                      type: object
-     *                      properties:
-     *                        name:
-     *                          type: string
-     *                          example: Walmart
-     *                        total_amount:
-     *                          type: float
-     *                          example: 12345.67
-     *        404:
-     *          description: 'Not Found'
-     *        500:
-     *          description: 'Server Error'
-     */
+    // GET data to populate a recipient's dashboard.
     this.router.get(`${this.path}/:id(\\w+)`, this.recController.getRecipientData);
   }
 }
