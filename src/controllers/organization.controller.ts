e507018cf06a39bceb4b02b76c9510ce1195d71e
Caleb <csuthe42@uwo.ca>
2022-03-24 19:52:56 -0400

    Change variable naming to camel case

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -1,6 +1,5 @@
 import OrganizationService from '@/services/organization.service';
 import { NextFunction, Request, Response } from 'express';
-import { start } from 'repl';
 import { HttpException } from '@exceptions/HttpException';
 
 class OrganizationController {
@@ -14,9 +13,9 @@ class OrganizationController {
         throw new HttpException(400, 'Please pass start and end date parameters.');
       }
 
-      const start_date = req.query.start_date.toString();
-      const end_date = req.query.end_date.toString();
-      const orgData = await this.organizationService.getOrganizationData(orgId, start_date, end_date);
+      const startDate = req.query.start_date.toString();
+      const endDate = req.query.end_date.toString();
+      const orgData = await this.organizationService.getOrganizationData(orgId, startDate, endDate);
       res.status(200).json(orgData);
     } catch (error) {
       next(error);
