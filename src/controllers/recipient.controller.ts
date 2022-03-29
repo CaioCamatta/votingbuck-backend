be2523a7826f6d2629c0d1b540a52cb7c5906294
Caleb <csuthe42@uwo.ca>
2022-03-29 14:56:33 -0400

    Add politician queries

--- src/controllers/recipient.controller.ts
+++ src/controllers/recipient.controller.ts
@@ -1,5 +1,6 @@
 import RecipientService from '@/services/recipient.service';
 import { NextFunction, Request, Response } from 'express';
+import { HttpException } from '@exceptions/HttpException';
 
 class RecipientController {
   public recipientService = new RecipientService();
@@ -7,8 +8,14 @@ class RecipientController {
   public getRecipientData = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const recId = req.params.id;
-      const recData = await this.recipientService.getRecipientData(recId);
 
+      if (!req.query.start_date || !req.query.end_date) {
+        throw new HttpException(400, 'Please pass start and end date parameters.');
+      }
+
+      const startDate = req.query.start_date.toString();
+      const endDate = req.query.end_date.toString();
+      const recData = await this.recipientService.getRecipientData(recId, startDate, endDate);
       res.status(200).json(recData);
     } catch (error) {
       next(error);
--- src/services/recipient.service.ts
+++ src/services/recipient.service.ts
@@ -1,10 +1,18 @@
-import { DonationsByMonth, Recipient, TopDonators } from '@interfaces/recipient.interface';
+import {
+  DonationsByMonth,
+  Recipient,
+  TopDonators,
+  IdeologyDistribution,
+  TopDonationsDollarsByIndustry,
+  TopDonationsDollarsByCorporation,
+  TopDonationsDollarsByUniversity,
+} from '@interfaces/recipient.interface';
 import { HttpException } from '@exceptions/HttpException';
 import { Prisma } from '@prisma/client';
 import prismaClient from '@databases/postgresClient';
 
 class RecipientService {
-  public async getRecipientData(recId: string): Promise<any> {
+  public async getRecipientData(recId: string, startDate: string, endDate: string): Promise<any> {
     // First check if rec exists (fetch recinfo such as name, industry)
     const recInfo: Recipient = await prismaClient.recipient.findUnique({
       where: {
@@ -16,8 +24,26 @@ class RecipientService {
       throw new HttpException(404, 'Recipient not found.');
     }
 
+    // Setup date objects to be used for query (start/end date for the requested period)
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
+
     // Then, proceed with queries
-    const [donationsByMonth, topDonators]: [DonationsByMonth, TopDonators] = [
+    const [
+      donationsByMonth,
+      topDonators,
+      ideologyDistribution,
+      topDonationsDollarsByIndustry,
+      topDonationsDollarsByCorporation,
+      topDonationsDollarsByUniversity,
+    ]: [
+      DonationsByMonth,
+      TopDonators,
+      IdeologyDistribution,
+      TopDonationsDollarsByIndustry,
+      TopDonationsDollarsByCorporation,
+      TopDonationsDollarsByUniversity,
+    ] = [
       // Note: Prisma's groupBy function is broken.
       // Donations received across time (grouped by month)
       await prismaClient.$queryRaw<DonationsByMonth>(
@@ -43,9 +69,72 @@ class RecipientService {
           ORDER  BY Sum(amount) DESC
           LIMIT  ${10};`,
       ),
+      // Get a distribution of a periods active politician's ideologies
+      await prismaClient.$queryRaw<IdeologyDistribution>(
+        Prisma.sql`
+          SELECT
+            CAST(ROUND(r.ideology, 2) as decimal(10,2)) as ideology,
+            COUNT(r.ideology) as count
+          FROM recipient AS r
+            JOIN office AS o
+              ON r.id = o.rec_id
+          WHERE r.id != ${recId} AND o.start_date <= ${startDateObj} AND o.end_date >= ${endDateObj} AND r.ideology IS NOT NULL
+          GROUP BY CAST(ROUND(r.ideology, 2) as decimal(10,2))
+          ORDER BY ideology ASC;`,
+      ),
+      // Get the top 5 industries that donate to this politician in this period
+      await prismaClient.$queryRaw<TopDonationsDollarsByIndustry>(
+        Prisma.sql`
+          SELECT
+            SUM(d.amount) as dollars_donated,
+            o.industry as industry
+          FROM donation AS d
+            JOIN organization AS o
+              ON d.org_id = o.id
+          WHERE d.rec_id = ${recId} AND o.industry != 'school' AND o.industry != 'School' AND d.date BETWEEN ${startDateObj} AND ${endDateObj}
+          GROUP BY industry
+          ORDER BY dollars_donated DESC
+          LIMIT ${5};`,
+      ),
+      // Get the top 5 corporations that donate to this politician in this period
+      await prismaClient.$queryRaw<TopDonationsDollarsByCorporation>(
+        Prisma.sql`
+          SELECT
+            SUM(d.amount) as dollars_donated,
+            o.name as corporation
+          FROM donation AS d
+            JOIN organization AS o
+              ON d.org_id = o.id
+          WHERE d.rec_id = ${recId} AND o.industry != 'school' AND o.industry != 'School' AND d.date BETWEEN ${startDateObj} AND ${endDateObj}
+          GROUP BY corporation
+          ORDER BY dollars_donated DESC
+          LIMIT ${5};`,
+      ),
+      // Get the top 5 universities that donate to this politician in this period
+      await prismaClient.$queryRaw<TopDonationsDollarsByUniversity>(
+        Prisma.sql`
+          SELECT
+            SUM(d.amount) as dollars_donated,
+            o.name as university
+          FROM donation AS d
+            JOIN organization AS o
+              ON d.org_id = o.id
+          WHERE d.rec_id = ${recId} AND (o.industry = 'school' OR o.industry = 'School') AND d.date BETWEEN ${startDateObj} AND ${endDateObj}
+          GROUP BY university
+          ORDER BY dollars_donated DESC
+          LIMIT ${5};`,
+      ),
     ];
 
-    return { recInfo, donationsByMonth, topDonators };
+    return {
+      recInfo,
+      donationsByMonth,
+      topDonators,
+      ideologyDistribution,
+      topDonationsDollarsByIndustry,
+      topDonationsDollarsByCorporation,
+      topDonationsDollarsByUniversity,
+    };
   }
 }
 
