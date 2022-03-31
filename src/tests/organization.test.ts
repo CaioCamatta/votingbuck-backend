32599b4b997c4055576cbac136aae90afe59cd56
Caleb <csuthe42@uwo.ca>
2022-03-31 18:51:12 -0400

    Fix tests

--- src/tests/organization.test.ts
+++ src/tests/organization.test.ts
@@ -7,7 +7,7 @@ afterAll(async () => {
 });
 
 describe('Testing Organizations', () => {
-  describe('[GET] /organizations/1?start_date=2017-11-03&end_date=2018-11-03', () => {
+  describe('[GET] /organizations/192108504?start_date=2017-11-03&end_date=2018-11-03', () => {
     it('returns status 200 and correct content', async () => {
       const orgRoute = new OrganizationRoute();
       const app = new App([orgRoute]);
